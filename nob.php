f0917505fda70b7f4b678576e428147bb81997b4
LCAsuperr3f <sony1928play@gmail.com>
2025-02-26 14:57:12 +0300

    Delete nob.php

--- nob.php
+++
@@ -1,9 +0,0 @@
-<?php
-// nob.php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $content = $_POST['content'];
-    echo "تم استلام المحتوى: " . htmlspecialchars($content);
-} else {
-    echo "الرجاء إرسال طلب POST";
-}
-?>
